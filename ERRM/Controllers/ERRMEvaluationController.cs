3ea026765214fccf813311ee9d119e3ab5f033ac
Dirk Eisenberg <dirk.eisenberg@gmail.com>
2026-03-12 05:40:02 +0100

    Initial Commit

---
+++ ERRM/Controllers/ERRMEvaluationController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ERRM.Controllers
+{
+    public class ERRMEvaluationController : Controller
+    {
+        // GET: ERRMEvaluationController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ ERRM/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ERRM.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
